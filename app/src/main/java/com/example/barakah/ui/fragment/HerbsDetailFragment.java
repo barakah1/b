0d0d972741b8013d1bcc65f814883459ca2ebf8e
ishu <ishu@apponward.com>
2020-03-09 01:42:02 +0530

    herbs task done

--- app/src/main/java/com/example/barakah/ui/fragment/HerbsDetailFragment.java
+++ app/src/main/java/com/example/barakah/ui/fragment/HerbsDetailFragment.java
@@ -1,5 +1,6 @@
 package com.example.barakah.ui.fragment;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -10,7 +11,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.barakah.R;
 import com.example.barakah.databinding.FragmentHerbsDetailBinding;
 import com.example.barakah.models.HerbsModel;
@@ -27,12 +31,13 @@ public class HerbsDetailFragment extends Fragment {
     public HerbsDetailFragment() {
         // Required empty public constructor
     }
+
     public static String TAG = "HerbsDetailFragment";
 
     public static HerbsDetailFragment newInstance(HerbsModel herbModel) {
         HerbsDetailFragment fragment = new HerbsDetailFragment();
         Bundle args = new Bundle();
-args.putSerializable(BarakahConstants.HERBS_MODEL,herbModel);
+        args.putSerializable(BarakahConstants.HERBS_MODEL, herbModel);
         fragment.setArguments(args);
         return fragment;
     }
@@ -40,9 +45,9 @@ args.putSerializable(BarakahConstants.HERBS_MODEL,herbModel);
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    if(getArguments()!=null){
-       herbsModel= (HerbsModel)getArguments().getSerializable(BarakahConstants.HERBS_MODEL);
-    }
+        if (getArguments() != null) {
+            herbsModel = (HerbsModel) getArguments().getSerializable(BarakahConstants.HERBS_MODEL);
+        }
     }
 
     @Override
@@ -55,15 +60,33 @@ args.putSerializable(BarakahConstants.HERBS_MODEL,herbModel);
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if(herbsModel!=null){
+        if (herbsModel != null) {
             setHerbsData(herbsModel);
+            binding.btnAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            binding.btnAddToFav.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
         }
 
     }
 
     public void setHerbsData(HerbsModel herbsData) {
+        binding.cardBenefit.setCardDescription(herbsData.getBenefit());
+        binding.cardConflict.setCardDescription(herbsData.getInteraction());
+        binding.cardEffects.setCardDescription(herbsData.getSide_effect());
+        binding.tvTitle.setText(herbsModel.getName());
+        binding.herbInfo.setText(herbsModel.getDescription());
+        Glide.with(getActivity()).load(herbsData.getImage()).into(binding.herbPic);
+
 
     }
-
 
 }
