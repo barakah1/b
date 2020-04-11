d2a8d4393bf23c63b39beee7e4f45e5051718867
Black4Guy <praveengautam3009@gmail.com>
2020-04-12 00:40:59 +0530

    added font

---
+++ app/src/main/java/com/example/barakah/utils/BarakahApp.java
@@ -0,0 +1,12 @@
+package com.example.barakah.utils;
+
+import android.app.Application;
+
+public class BarakahApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TypefaceUtil.overrideFont(getApplicationContext(), "fonts/tajawal.ttf", "fonts/tajawal.ttf"); // font from assets: "assets/fonts/Roboto-Regular.ttf
+    }
+}
