d8a48d8f2961df6222126ba7a6d09e74b1ea85b2
ishu <ishu@apponward.com>
2020-04-26 12:34:48 +0530

    code merge

--- app/src/main/java/com/example/barakah/ui/fragment/ChangePasswordFragment.java
+++ app/src/main/java/com/example/barakah/ui/fragment/ChangePasswordFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,8 @@ import com.example.barakah.utils.BarakahUtils;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -28,6 +31,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class ChangePasswordFragment extends Fragment {
     public static String TAG = "ChangePasswordFragment";
 
+    private TextInputEditText etEmail;
     private TextInputEditText etPass;
     private TextInputEditText etConfirmPass;
     private Dialog progressDialog;
@@ -65,6 +69,7 @@ public class ChangePasswordFragment extends Fragment {
     }
 
     private void initialization(View view) {
+        etEmail = view.findViewById(R.id.etEmail);
         etPass = view.findViewById(R.id.etPass);
         etConfirmPass = view.findViewById(R.id.etConfirmPass);
         submit = view.findViewById(R.id.btnSubmit);
@@ -82,24 +87,65 @@ public class ChangePasswordFragment extends Fragment {
     }
 
     private void checkValidation() {
-        String email = etPass.getText().toString().trim();
-        String password = etConfirmPass.getText().toString().trim();
+        String email = etEmail.getText().toString().trim();
+        String password = etPass.getText().toString().trim();
+        String newPassword = etConfirmPass.getText().toString().trim();
         if (email.isEmpty()) {
-            Toast.makeText(getActivity(), getResources().getString(R.string.pass_req), Toast.LENGTH_SHORT).show();
-        } else if (password.isEmpty()) {
-            Toast.makeText(getActivity(), getResources().getString(R.string.confirm_pass_req), Toast.LENGTH_SHORT).show();
-        } else if (!email.equals(password)) {
-            Toast.makeText(getActivity(), getResources().getString(R.string.confirm_pass_not_matched), Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), getResources().getString(R.string.email_req), Toast.LENGTH_SHORT).show();
+        } else if (!BarakahUtils.isValidEmailId(email)) {
+            Toast.makeText(getActivity(), getResources().getString(R.string.invalid_email), Toast.LENGTH_SHORT).show();
+        }else if (password.isEmpty()) {
+            Toast.makeText(getActivity(), getResources().getString(R.string.plz_enter_old_pass), Toast.LENGTH_SHORT).show();
+        } else if (newPassword.isEmpty()) {
+            Toast.makeText(getActivity(), getResources().getString(R.string.plz_enter_new_pass), Toast.LENGTH_SHORT).show();
         } else {
             progressDialog = BarakahUtils.customProgressDialog(getActivity());
-            login(password);
+            login(email,password,newPassword);
         }
     }
 
-    private void login(String password) {
-        final FirebaseUser user = mAuth.getCurrentUser();
+    private void login(String email, String pass, final String newPass) {
+      //  final FirebaseUser user = mAuth.getCurrentUser();
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        user.updatePassword(password).addOnCompleteListener(new OnCompleteListener<Void>() {
+// Get auth credentials from the user for re-authentication. The example below shows
+// email and password credentials but there are multiple possible providers,
+// such as GoogleAuthProvider or FacebookAuthProvider.
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(email, pass);
+
+// Prompt the user to re-provide their sign-in credentials
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            user.updatePassword(newPass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        closeProgress();
+
+                                        Toast.makeText(getActivity(), getResources().getString(R.string.pass_change_success), Toast.LENGTH_SHORT).show();
+                                        getActivity().finish();
+
+                                    } else {
+                                        closeProgress();
+
+                                        Toast.makeText(getActivity(), getResources().getString(R.string.pass_update_fail), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            closeProgress();
+
+                            Toast.makeText(getActivity(), getResources().getString(R.string.invalid_credentials), Toast.LENGTH_SHORT).show();
+
+                            Log.d(TAG, "Error auth failed");
+                        }
+                    }
+                });
+     /*   user.updatePassword(password).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()) {
@@ -109,7 +155,7 @@ public class ChangePasswordFragment extends Fragment {
                     getActivity().finish();
                 } else closeProgress();
             }
-        });
+        });*/
     }
 
 
